cc95fe56c51e7de4d60894b8c2a81ec6db3017fd
Harold Torres Marino <haroldtm55@gmail.com>
2022-04-24 10:19:19 +1000

    Rendering a list of dummy meetups

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image: 'https://www.peerspace.com/resources/wp-content/uploads/picnic-1208229_1280-1200x600.webp',
+    address: '123 main st, some suburb',
+    description: 'This is a first meetup'
+  },
+  {
+    id: 'm2',
+    title: 'A second meetup',
+    image: 'https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1636558970768-NS81KU5BXCPZIPFTJ09P/unsplash-image-3SBfM2-ytb8.jpg',
+    address: '456 secondary st, some suburb',
+    description: 'This is a second meetup'
+  }
+]
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
