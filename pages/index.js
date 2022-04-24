9ff9c864ca221dece30647f44c21819c4b1a142a
Harold Torres Marino <haroldtm55@gmail.com>
2022-04-25 08:35:07 +1000

    Getting Data From The Database (For Page Pre-Rendering)

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
-import { useEffect, useState } from "react"
+import MongoClient from "mongodb/lib/mongo_client"
 import MeetupList from "../components/meetups/MeetupList"
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image: 'https://www.peerspace.com/resources/wp-content/uploads/picnic-1208229_1280-1200x600.webp',
-    address: '123 main st, some suburb',
-    description: 'This is a first meetup'
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image: 'https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1636558970768-NS81KU5BXCPZIPFTJ09P/unsplash-image-3SBfM2-ytb8.jpg',
-    address: '456 secondary st, some suburb',
-    description: 'This is a second meetup'
-  }
-]
 
 function HomePage({meetups}) {
   return <MeetupList meetups={meetups} />
@@ -35,9 +18,22 @@ function HomePage({meetups}) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  
+  const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a28iu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+  const db = client.db()
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find().toArray()
+  client.close()
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString()
+      }))
     },
     revalidate: 1
   }
